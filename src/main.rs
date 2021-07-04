bd2984b03a4b6027e41a9f4794f276328d42aaa2
Hyde46 <denis.heid@protonmail.com>
2021-07-04 14:48:17 +0000

    Basic architecture for new command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use clap::{App, load_yaml};
+
 fn main() {
-    println!("Welcome to hoard!");
+    // The YAML file is found relative to the current file, similar to how modules are found
+    let yaml = load_yaml!("cli.yaml");
+    let matches = App::from(yaml).get_matches();
+    println!("{:?}", matches)
+    // Same as previous examples...
 }
