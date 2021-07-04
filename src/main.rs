c494f62faf5191a423c85cdb457ffc2b7a045ea0
Hyde46 <denis.heid@protonmail.com>
2021-07-04 17:52:13 +0200

    Move cli yaml file

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use clap::{App, load_yaml};
 
 fn main() {
     // The YAML file is found relative to the current file, similar to how modules are found
-    let yaml = load_yaml!("cli.yaml");
+    let yaml = load_yaml!("resources/cli.yaml");
     let matches = App::from(yaml).get_matches();
     println!("{:?}", matches)
     // Same as previous examples...
