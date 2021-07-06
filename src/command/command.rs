420c2691357a671ad252136b6d162a30f4985b32
Victor Künstler <victor.kuenstler@outlook.com>
2021-07-06 19:16:18 +0000

    Restrucuture

--- src/command/command.rs
+++ src/command/command.rs
@@ -1,5 +1,39 @@
-pub trait Command {
-    fn new(matches: &&clap::ArgMatches) -> Self;
-    fn default() -> Self;
-    fn is_complete(&self) -> bool;
+use super::argument::Argument;
+
+#[derive(Debug)]
+pub struct Command {
+    pub name: String,
+    pub arguments: Vec<Argument>,
+    is_complete: bool,
+}
+
+impl Command {
+    pub fn new(name: String, arguments: Vec<Argument>) -> Self {
+        Self {
+            name,
+            arguments,
+            is_complete: true,
+        }
+    }
+
+    pub fn populate(&mut self, matches: &&clap::ArgMatches) -> &mut Command {
+        for argument in self.arguments.iter_mut() {
+            if let Some(n) = matches.value_of(argument.name()) {
+                argument.set_value(Some(n.to_string()));
+                argument.validate()
+            } else {
+                self.is_complete = false;
+            }
+        }
+        self
+    }
+
+    pub fn complete_interactive(&mut self) -> &mut Command {
+        for argument in self.arguments.iter_mut() {
+            if argument.value().is_none() {
+                argument.interactive();
+            }
+        }
+        self
+    }
 }
--- src/command/mod.rs
+++ src/command/mod.rs
@@ -1,2 +1,2 @@
+pub mod argument;
 pub mod command;
-pub mod new_command;
