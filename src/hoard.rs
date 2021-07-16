94f85da924eb3f085447a39715ab7468124be726
Hyde46 <denis.heid@protonmail.com>
2021-07-16 20:16:26 +0000

    Improve how input for commands is expected

--- src/hoard.rs
+++ src/hoard.rs
@@ -1,7 +1,7 @@
 use clap::{load_yaml, App};
 use log::{info, warn};
 
-use crate::config::load_or_build_config;
+use crate::{command::hoard_command::Parsable, config::load_or_build_config};
 
 use dialoguer::{theme::ColorfulTheme, Input};
 
@@ -41,28 +41,6 @@ impl Hoard {
         self
     }
 
-    pub fn with_example_command(&mut self) -> &mut Self {
-        let example_command = HoardCommand {
-            name: Some(String::from("example")),
-            namespace: Some(String::from("default")),
-            tags: Some(vec![String::from("default"), String::from("example")]),
-            command: Some(String::from("'cd ..'")),
-        };
-        let trove = CommandTrove {
-            version: String::from("0.1.0"),
-            commands: vec![example_command],
-        };
-        let s = serde_yaml::to_string(&trove).unwrap();
-        let example_path = PathBuf::from(".trove.yml");
-        fs::write(example_path.clone(), s).expect("Unable to write config file");
-
-        let f = std::fs::File::open(example_path).unwrap();
-        let loaded_trove: CommandTrove = serde_yaml::from_reader::<_, CommandTrove>(f).unwrap();
-        info!("{:?}", loaded_trove);
-
-        self
-    }
-
     pub fn load_trove(&mut self) -> &mut Self {
         match &self.config {
             Some(config) => {
@@ -90,38 +68,20 @@ impl Hoard {
 
         match matches.subcommand_name() {
             Some("new") => {
-                // WIP
-                let command_string: String = Input::with_theme(&ColorfulTheme::default())
-                    .with_prompt("Command to hoard")
-                    .interact_text()
-                    .unwrap();
-                let command_name: String = Input::with_theme(&ColorfulTheme::default())
-                    .with_prompt("Name the command")
-                    .interact_text()
-                    .unwrap();
-                let command_tags: String = Input::with_theme(&ColorfulTheme::default())
-                    .with_prompt("Set tags for the new command")
-                    .interact_text()
-                    .unwrap();
-                let namespace: String = Input::with_theme(&ColorfulTheme::default())
-                    .with_prompt("Set the namespace")
-                    .interact_text()
-                    .unwrap();
+                let new_command = HoardCommand::default()
+                    .with_command_string_input()
+                    .with_name_input()
+                    .with_description_input()
+                    .with_tags_input()
+                    .with_namespace_input();
+                self.trove.add_command(new_command);
+                self.save_trove();
             }
             // Rest of subcommands go here for now
             _ => {
                 println!("Not implemented yet!")
             }
         }
-        // Only testing purposes.
-        // How to add a new command to the trove
-        self.trove.add_command(HoardCommand {
-            name: Some(String::from("example")),
-            namespace: Some(String::from("default")),
-            tags: Some(vec![String::from("default"), String::from("example")]),
-            command: Some(String::from("'cd ..'")),
-        });
-        self.save_trove();
 
         Ok(())
     }
