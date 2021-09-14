8c8b51ebbae278805127b7a082f2845288ff4396
Denis <denis.heid@protonmail.com>
2021-09-14 14:48:32 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -56,14 +56,12 @@ __hoard_install_mac(){
 
 __hoard_detect_os(){
 
-    echo "Packaged version is out of date, please install hoard from source with cargo"
-    exit
-    #case "$OSTYPE" in
-    #    linux*) __hoard_install_ubuntu ;;
-    #    #linux*) __hoard_install_linux ;;
-    #    darwin*) __hoard_install_mac ;;
-    #    #*)        __hoard_install_unsupported ;;
-    #esac
+    case "$OSTYPE" in
+        linux*) __hoard_install_ubuntu ;;
+        #linux*) __hoard_install_linux ;;
+        darwin*) __hoard_install_mac ;;
+        #*)        __hoard_install_unsupported ;;
+    esac
 }
 
 # Set default installation location
