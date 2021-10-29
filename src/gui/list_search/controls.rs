aa2e69f0a811bde366569c597e5ae479790d6fb1
qb60 <q.basic60@gmail.com>
2021-10-29 17:13:00 +0500

    ✨ Help page in GUI (#66)
    * feat: help screen
    
    * ref: decoupling commands_gui.rs
    
    * test: tests for list search controls
    
    * ref: list search controls cleanup
    
    * fix: draw method name
    
    * fix: help hint fix

---
+++ src/gui/list_search/controls.rs
@@ -0,0 +1,370 @@
+use crate::command::hoard_command::{HoardCommand, Parameterized};
+use crate::gui::commands_gui::{DrawState, State};
+use termion::event::Key;
+
+pub fn key_handler(
+    input: Key,
+    state: &mut State,
+    trove_commands: &[HoardCommand],
+    namespace_tabs: &[&str],
+) -> Option<HoardCommand> {
+    match input {
+        // Quit command
+        Key::Esc | Key::Ctrl('c' | 'd' | 'g') => {
+            state.should_exit = true;
+            None
+        }
+        // Show help
+        Key::F(1) => {
+            state.draw_state = DrawState::Help;
+            None
+        }
+        // Switch namespace
+        Key::Left | Key::Ctrl('h') => {
+            if let Some(selected) = state.namespace_tab_state.selected() {
+                let new_selected_tab = previous_index(selected, namespace_tabs.len());
+                switch_namespace(state, new_selected_tab, namespace_tabs, trove_commands);
+            }
+            None
+        }
+        Key::Right | Key::Ctrl('l') => {
+            if let Some(selected) = state.namespace_tab_state.selected() {
+                let new_selected_tab = next_index(selected, namespace_tabs.len());
+                switch_namespace(state, new_selected_tab, namespace_tabs, trove_commands);
+            }
+            None
+        }
+        // Switch command
+        Key::Up | Key::Ctrl('y' | 'p') => {
+            if !state.commands.is_empty() {
+                if let Some(selected) = state.command_list_state.selected() {
+                    let new_selected = previous_index(selected, state.commands.len());
+                    state.command_list_state.select(Some(new_selected));
+                }
+            }
+            None
+        }
+        Key::Down | Key::Ctrl('.' | 'n') => {
+            if !state.commands.is_empty() {
+                if let Some(selected) = state.command_list_state.selected() {
+                    let new_selected = next_index(selected, state.commands.len());
+                    state.command_list_state.select(Some(new_selected));
+                }
+            }
+            None
+        }
+        // Select command
+        Key::Char('\n') => {
+            if state.commands.is_empty() {
+                state.should_exit = true;
+                return None;
+            }
+            let selected_command = state
+                .commands
+                .clone()
+                .get(
+                    state
+                        .command_list_state
+                        .selected()
+                        .expect("there is always a selected command"),
+                )
+                .expect("exists")
+                .clone();
+
+            // Check if parameters need to be supplied
+            if selected_command.get_parameter_count(&state.parameter_token) > 0 {
+                // Set next state to draw
+                state.draw_state = DrawState::ParameterInput;
+                // Save which command to replace parameters for
+                state.selected_command = Some(selected_command);
+                // Empty input for next screen
+                state.input = "".to_string();
+                // return None, otherwise drawing will quit
+                return None;
+            }
+            Some(selected_command)
+        }
+        // Handle query input
+        Key::Backspace => {
+            state.input.pop();
+            apply_filter(state, namespace_tabs, trove_commands);
+            None
+        }
+        Key::Char(c) => {
+            state.input.push(c);
+            apply_filter(state, namespace_tabs, trove_commands);
+            None
+        }
+        _ => None,
+    }
+}
+
+const fn next_index(current_index: usize, collection_length: usize) -> usize {
+    if current_index >= collection_length - 1 {
+        0
+    } else {
+        current_index + 1
+    }
+}
+
+const fn previous_index(current_index: usize, collection_length: usize) -> usize {
+    if current_index > 0 {
+        current_index - 1
+    } else {
+        collection_length - 1
+    }
+}
+
+fn switch_namespace(
+    state: &mut State,
+    index_to_select: usize,
+    namespaces: &[&str],
+    commands: &[HoardCommand],
+) {
+    state.namespace_tab_state.select(Some(index_to_select));
+
+    let selected_namespace = namespaces
+        .get(index_to_select)
+        .expect("Always a tab selected");
+
+    apply_search(state, commands, selected_namespace);
+
+    let new_selected_command = if state.commands.is_empty() {
+        0
+    } else {
+        state.commands.len() - 1
+    };
+
+    state.command_list_state.select(Some(new_selected_command));
+}
+
+fn apply_search(state: &mut State, all_commands: &[HoardCommand], selected_tab: &str) {
+    let query_term = &state.input[..];
+    state.commands = all_commands
+        .to_owned()
+        .into_iter()
+        .filter(|c| {
+            (c.name.contains(query_term)
+                || c.namespace.contains(query_term)
+                || c.tags_as_string().contains(query_term)
+                || c.command.contains(query_term)
+                || c.description
+                    .clone()
+                    .unwrap_or_default()
+                    .contains(query_term))
+                && (c.namespace.clone() == *selected_tab || selected_tab == "All")
+        })
+        .collect();
+}
+
+fn apply_filter(state: &mut State, namespaces: &[&str], commands: &[HoardCommand]) {
+    let selected_tab = namespaces
+        .get(
+            state
+                .namespace_tab_state
+                .selected()
+                .expect("Always a namespace selected"),
+        )
+        .expect("Always a tab selected");
+    apply_search(state, commands, selected_tab);
+}
+
+#[cfg(test)]
+mod test_controls {
+    use super::*;
+    use tui::widgets::ListState;
+
+    const DEFAULT_NAMESPACE: &str = "default";
+
+    fn create_command(name: &str, namespace: &str) -> HoardCommand {
+        HoardCommand {
+            name: name.to_string(),
+            namespace: namespace.to_string(),
+            tags: None,
+            command: name.to_string(),
+            description: None,
+        }
+    }
+
+    fn create_state(commands: Vec<HoardCommand>) -> State {
+        let mut state = State {
+            input: "".to_string(),
+            commands,
+            command_list_state: ListState::default(),
+            namespace_tab_state: ListState::default(),
+            should_exit: false,
+            draw_state: DrawState::Search,
+            parameter_token: "#".to_string(),
+            selected_command: None,
+            provided_parameter_count: 0,
+        };
+
+        state.command_list_state.select(Some(0));
+        state.namespace_tab_state.select(Some(0));
+
+        state
+    }
+
+    fn test_change_command(key: Key, initial_index: usize, expected_index: usize) {
+        let namespaces = vec![DEFAULT_NAMESPACE];
+        let cmd1 = create_command("first", DEFAULT_NAMESPACE);
+        let cmd2 = create_command("second", DEFAULT_NAMESPACE);
+        let cmd3 = create_command("third", DEFAULT_NAMESPACE);
+        let mut state = create_state(vec![cmd1, cmd2, cmd3]);
+        state.command_list_state.select(Some(initial_index));
+
+        let commands = state.commands.clone();
+        key_handler(key, &mut state, &commands, &namespaces);
+        let new_selected_index = state.command_list_state.selected();
+
+        assert_eq!(expected_index, new_selected_index.unwrap());
+    }
+
+    fn test_change_namespace(key: Key, initial_index: usize, expected_index: usize) {
+        let namespaces = vec!["first", "second", "third"];
+        let mut state = create_state(vec![]);
+        state.namespace_tab_state.select(Some(initial_index));
+
+        let commands = state.commands.clone();
+        key_handler(key, &mut state, &commands, &namespaces);
+        let new_selected_index = state.namespace_tab_state.selected();
+
+        assert_eq!(expected_index, new_selected_index.unwrap());
+    }
+
+    // Commands
+    #[test]
+    fn next_command() {
+        test_change_command(Key::Down, 0, 1);
+    }
+
+    #[test]
+    fn next_command_wrap() {
+        test_change_command(Key::Down, 2, 0);
+    }
+
+    #[test]
+    fn previous_command() {
+        test_change_command(Key::Up, 2, 1);
+    }
+
+    #[test]
+    fn previous_command_wrap() {
+        test_change_command(Key::Up, 0, 2);
+    }
+
+    // Namespaces
+    #[test]
+    fn next_namespace() {
+        test_change_namespace(Key::Right, 1, 2);
+    }
+
+    #[test]
+    fn next_namespace_wrap() {
+        test_change_namespace(Key::Right, 2, 0);
+    }
+
+    #[test]
+    fn previous_namespace() {
+        test_change_namespace(Key::Left, 2, 1);
+    }
+
+    #[test]
+    fn previous_namespace_wrap() {
+        test_change_namespace(Key::Left, 0, 2);
+    }
+
+    #[test]
+    fn filter_commands_when_namespace_changed() {
+        let namespace1 = "first_namespace";
+        let namespace2 = "second_namespace";
+        let all_namespaces = vec![namespace1, namespace2];
+
+        let cmd2_name = "second_command";
+        let cmd1 = create_command("first_command", namespace1);
+        let cmd2 = create_command(cmd2_name, namespace2);
+        let mut state = create_state(vec![cmd1, cmd2]);
+
+        let commands = state.commands.clone();
+        key_handler(Key::Right, &mut state, &commands, &all_namespaces);
+        let filtered_commands = state.commands;
+
+        assert_eq!(1, filtered_commands.len());
+        assert_eq!(cmd2_name, filtered_commands.first().unwrap().name);
+    }
+
+    #[test]
+    fn select_last_command_when_namespace_changed() {
+        let namespace1 = "first_namespace";
+        let namespace2 = "second_namespace";
+        let all_namespaces = vec![namespace1, namespace2];
+
+        let expected_command_index = 1;
+        let cmd1 = create_command("first_command", namespace2);
+        let cmd2 = create_command("second_command", namespace2);
+        let mut state = create_state(vec![cmd1, cmd2]);
+
+        let commands = state.commands.clone();
+        key_handler(Key::Right, &mut state, &commands, &all_namespaces);
+        let selected_command_index = state.command_list_state.selected().unwrap();
+
+        assert_eq!(expected_command_index, selected_command_index);
+    }
+
+    #[test]
+    fn pick_command_without_params() {
+        let namespaces = vec![DEFAULT_NAMESPACE];
+        let expected_command = "second_command";
+        let command_index = 1;
+        let cmd1 = create_command("first_command", DEFAULT_NAMESPACE);
+        let cmd2 = create_command(expected_command, DEFAULT_NAMESPACE);
+
+        let mut state = create_state(vec![cmd1, cmd2]);
+        state.command_list_state.select(Some(command_index));
+
+        let commands = state.commands.clone();
+        let actual_command =
+            key_handler(Key::Char('\n'), &mut state, &commands, &namespaces).unwrap();
+
+        assert_eq!(expected_command, actual_command.command);
+    }
+
+    #[test]
+    fn pick_command_with_params() {
+        let namespaces = vec![DEFAULT_NAMESPACE];
+        let cmd = create_command("first_command #", DEFAULT_NAMESPACE);
+
+        let mut state = create_state(vec![cmd]);
+        let commands = state.commands.clone();
+        key_handler(Key::Char('\n'), &mut state, &commands, &namespaces);
+
+        assert_eq!(DrawState::ParameterInput, state.draw_state);
+    }
+
+    #[test]
+    fn quit_on_nothing_to_pick() {
+        let mut state = create_state(vec![]);
+
+        key_handler(Key::Char('\n'), &mut state, &[], &[]);
+
+        assert!(state.should_exit);
+    }
+
+    #[test]
+    fn quit() {
+        let mut state = create_state(vec![]);
+
+        key_handler(Key::Esc, &mut state, &[], &[]);
+
+        assert!(state.should_exit);
+    }
+
+    #[test]
+    fn show_help() {
+        let mut state = create_state(vec![]);
+
+        key_handler(Key::F(1), &mut state, &[], &[]);
+
+        assert_eq!(DrawState::Help, state.draw_state);
+    }
+}
---
+++ src/gui/list_search/render.rs
@@ -0,0 +1,258 @@
+use crate::command::hoard_command::HoardCommand;
+use crate::config::HoardConfig;
+use crate::gui::commands_gui::State;
+use crate::gui::help::HELP_KEY;
+use termion::screen::AlternateScreen;
+use tui::backend::TermionBackend;
+use tui::layout::{Alignment, Constraint, Direction, Layout};
+use tui::style::{Color, Modifier, Style};
+use tui::text::{Span, Spans};
+use tui::widgets::{Block, BorderType, Borders, List, ListItem, Paragraph, Tabs, Wrap};
+use tui::Terminal;
+
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+pub fn draw(
+    app_state: &mut State,
+    config: &HoardConfig,
+    namespace_tabs: &mut Vec<&str>,
+    terminal: &mut Terminal<
+        TermionBackend<AlternateScreen<termion::raw::RawTerminal<std::io::Stdout>>>,
+    >,
+) -> Result<(), eyre::Error> {
+    terminal.draw(|rect| {
+        let size = rect.size();
+        let chunks = Layout::default()
+            .direction(Direction::Vertical)
+            .margin(1)
+            .constraints(
+                [
+                    Constraint::Length(3),
+                    Constraint::Min(2),
+                    Constraint::Length(3),
+                    Constraint::Length(1),
+                ]
+                .as_ref(),
+            )
+            .split(size);
+        let menu = namespace_tabs
+            .iter()
+            .map(|t| {
+                Spans::from(vec![Span::styled(
+                    *t,
+                    Style::default().fg(Color::Rgb(
+                        config.primary_color.unwrap().0,
+                        config.primary_color.unwrap().1,
+                        config.primary_color.unwrap().2,
+                    )),
+                )])
+            })
+            .collect();
+
+        let tabs = Tabs::new(menu)
+            .select(
+                app_state
+                    .namespace_tab_state
+                    .selected()
+                    .expect("Always a namespace selected"),
+            )
+            .block(
+                Block::default()
+                    .title(" Hoard Namespace ")
+                    .borders(Borders::ALL),
+            )
+            .style(Style::default().fg(Color::Rgb(
+                config.primary_color.unwrap().0,
+                config.primary_color.unwrap().1,
+                config.primary_color.unwrap().2,
+            )))
+            .highlight_style(
+                Style::default()
+                    .fg(Color::Rgb(181, 118, 20))
+                    .add_modifier(Modifier::UNDERLINED),
+            )
+            .divider(Span::raw("|"));
+
+        rect.render_widget(tabs, chunks[0]);
+
+        let commands_chunks = Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints([Constraint::Percentage(30), Constraint::Percentage(70)].as_ref())
+            .split(chunks[1]);
+        let command_detail_chunks = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints(
+                [
+                    Constraint::Length(3),
+                    Constraint::Percentage(60),
+                    Constraint::Length(3),
+                ]
+                .as_ref(),
+            )
+            .split(commands_chunks[1]);
+        let (commands, command, tags_widget, description, input) =
+            render_commands(&app_state.commands.clone(), app_state, config);
+        rect.render_stateful_widget(
+            commands,
+            commands_chunks[0],
+            &mut app_state.command_list_state,
+        );
+        rect.render_widget(tags_widget, command_detail_chunks[0]);
+        rect.render_widget(description, command_detail_chunks[1]);
+        rect.render_widget(command, command_detail_chunks[2]);
+        rect.render_widget(input, chunks[2]);
+
+        let help_hint = Paragraph::new(format!("Show help - {} ", HELP_KEY))
+            .style(Style::default().fg(Color::Rgb(
+                config.primary_color.unwrap().0,
+                config.primary_color.unwrap().1,
+                config.primary_color.unwrap().2,
+            )))
+            .alignment(Alignment::Right);
+
+        rect.render_widget(help_hint, chunks[3]);
+    })?;
+    Ok(())
+}
+
+#[allow(clippy::too_many_lines)]
+fn render_commands<'a>(
+    commands_list: &[HoardCommand],
+    app: &mut State,
+    config: &HoardConfig,
+) -> (
+    List<'a>,
+    Paragraph<'a>,
+    Paragraph<'a>,
+    Paragraph<'a>,
+    Paragraph<'a>,
+) {
+    let commands = Block::default()
+        .borders(Borders::ALL)
+        .style(Style::default().fg(Color::Rgb(
+            config.primary_color.unwrap().0,
+            config.primary_color.unwrap().1,
+            config.primary_color.unwrap().2,
+        )))
+        .title(" Commands ")
+        .border_type(BorderType::Plain);
+
+    let items: Vec<_> = commands_list
+        .iter()
+        .map(|command| {
+            ListItem::new(Spans::from(vec![Span::styled(
+                command.name.clone(),
+                Style::default(),
+            )]))
+        })
+        .collect();
+
+    let selected_command: HoardCommand = commands_list
+        .get(
+            app.command_list_state
+                .selected()
+                .expect("there is always a selected command"),
+        )
+        .get_or_insert(&HoardCommand::default())
+        .clone();
+
+    if selected_command.name.is_empty() {
+        // If somehow the selection is past the last index, set it to the last element
+        let new_selection = if commands_list.is_empty() {
+            0
+        } else {
+            commands_list.len() - 1
+        };
+        app.command_list_state.select(Some(new_selection));
+    }
+
+    let list = List::new(items).block(commands).highlight_style(
+        Style::default()
+            .bg(Color::Rgb(
+                config.secondary_color.unwrap().0,
+                config.secondary_color.unwrap().1,
+                config.secondary_color.unwrap().2,
+            ))
+            .fg(Color::Rgb(
+                config.tertiary_color.unwrap().0,
+                config.tertiary_color.unwrap().1,
+                config.tertiary_color.unwrap().2,
+            ))
+            .add_modifier(Modifier::BOLD),
+    );
+
+    let command = Paragraph::new(selected_command.command.clone())
+        .style(Style::default().fg(Color::Rgb(
+            config.command_color.unwrap().0,
+            config.command_color.unwrap().1,
+            config.command_color.unwrap().2,
+        )))
+        .alignment(Alignment::Left)
+        .wrap(Wrap { trim: true })
+        .block(
+            Block::default()
+                .borders(Borders::ALL)
+                .style(Style::default().fg(Color::Rgb(
+                    config.primary_color.unwrap().0,
+                    config.primary_color.unwrap().1,
+                    config.primary_color.unwrap().2,
+                )))
+                .title(" Hoarded command ")
+                .border_type(BorderType::Plain),
+        );
+
+    let tags = Paragraph::new(selected_command.tags_as_string())
+        .style(Style::default().fg(Color::Rgb(
+            config.primary_color.unwrap().0,
+            config.primary_color.unwrap().1,
+            config.primary_color.unwrap().2,
+        )))
+        .alignment(Alignment::Left)
+        .block(
+            Block::default()
+                .borders(Borders::ALL)
+                .style(Style::default().fg(Color::Rgb(
+                    config.primary_color.unwrap().0,
+                    config.primary_color.unwrap().1,
+                    config.primary_color.unwrap().2,
+                )))
+                .title(" Tags ")
+                .border_type(BorderType::Plain),
+        );
+
+    let description = Paragraph::new(selected_command.description.unwrap_or_default())
+        .style(Style::default().fg(Color::Rgb(
+            config.primary_color.unwrap().0,
+            config.primary_color.unwrap().1,
+            config.primary_color.unwrap().2,
+        )))
+        .alignment(Alignment::Left)
+        .wrap(Wrap { trim: true })
+        .block(
+            Block::default()
+                .borders(Borders::ALL)
+                .style(Style::default().fg(Color::Rgb(
+                    config.primary_color.unwrap().0,
+                    config.primary_color.unwrap().1,
+                    config.primary_color.unwrap().2,
+                )))
+                .title(" Description ")
+                .border_type(BorderType::Plain),
+        );
+
+    let mut query_string = config.query_prefix.clone();
+    query_string.push_str(&app.input.clone()[..]);
+    let query_title = format!(" hoard v{} ", VERSION);
+    let input = Paragraph::new(query_string).block(
+        Block::default()
+            .style(Style::default().fg(Color::Rgb(
+                config.primary_color.unwrap().0,
+                config.primary_color.unwrap().1,
+                config.primary_color.unwrap().2,
+            )))
+            .borders(Borders::ALL)
+            .title(query_title),
+    );
+
+    (list, command, tags, description, input)
+}
